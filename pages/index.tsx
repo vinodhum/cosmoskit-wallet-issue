601cfca730e4c7bbbabce3c147320bc7fb997a72
Vinod Hum <vinodhum@outlook.com>
2024-01-12 12:58:47 -0500

    show address

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
 import { useChain } from "@cosmos-kit/react";
 
 export default function Home() {
-  const { connect, disconnect, isWalletConnected } = useChain("migaloo");
+  const { connect, disconnect, isWalletConnected , address} = useChain("migaloo");
 
   return (
     <main>
       <h1>Next.js + TypeScript</h1>
+      <p>{address}</p>
       {isWalletConnected ? (
-        <button onClick={disconnect}>Disconnect</button>
+        <button onClick={() => disconnect()}>Disconnect</button>
       ) : (
         <button onClick={connect}>Connect</button>
       )}
